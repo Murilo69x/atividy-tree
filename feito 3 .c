0debec10b4be9622a4e148863f47dcd342da90f9
Murilo69x <muriloolliveira8@gmail.com>
2026-04-01 20:40:51 -0300

    Create feito 3 .c

---
+++ feito 3 .c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    float nota;
+    printf( "qual foi sua nota? ");
+    scanf("%f", &nota);
+    
+    if (nota >7){
+    printf("\naprovado\n");
+}
+    else if (nota >=5){
+    printf("recuperação\n");
+}        
+    else {
+    printf("reprovado\n");
+}
+    return 0;    scanf("%d", &nota);
+
+
+
+}
